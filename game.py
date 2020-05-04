8d08c733be49faa0f38f7c5d415af86f10541972
SAMDIMAS123 <samueldimas18@gmail.com>
2020-05-04 22:19:14 +0200

    first commit

---
+++ game.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+
+screen = pygame.display.set_mode( size = (WIDTH, HEIGHT))
+
+game_over = False
+
+while not game_over: 
+    for event in pygame.event.get():
+        print(event)
+
+    if event.type == pygame.QUIT:
+        sys.exit()
---
+++ readme.md
@@ -0,0 +1,1 @@
+tutorial python game https://youtu.be/-8n91btt5d8
